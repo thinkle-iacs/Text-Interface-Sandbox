295ff6e57dfb7ec36f8aec654f12a270ffcc3fda
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-03-22 14:20:57 +0000

    ++images

--- textInterface/index.ts
+++ textInterface/index.ts
@@ -1,3 +1,7 @@
+import { TextInterface } from './textInterface';
 import './textInterface.css'
 export {TextInterface} from './textInterface';
 
+const ti = new TextInterface();
+
+ti.showHTML('<div>Hello World</div>')
