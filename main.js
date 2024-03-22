d92bac0d81223714e1cc5cfc69bd2838d5d93154
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-03-22 14:54:20 +0000

    vscode boilerplate

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ import {TextInterface} from './textInterface';
 
 let app = document.querySelector('#app');
 // Create a new "Text Interface"
-let ti = new TextInterface(app, "Example Text Game");
+let ti = new TextInterface(app, "Example Text Interface");
 ti.output("What is your name?");
 let name = await ti.readText()
-ti.output("Hello, "+name);
-ti.output("Here is a pretty mountain:");
-ti.showImage("https://upload.wikimedia.org/wikipedia/commons/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg");
+await ti.output("Hello, "+name);
+await ti.output("Here is a pretty mountain:");
+await ti.showImage("https://upload.wikimedia.org/wikipedia/commons/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg");
+await ti.readText();
