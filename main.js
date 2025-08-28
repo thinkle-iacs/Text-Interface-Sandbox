fc6ea128f8ed9ea132966cddf229825e58322488
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2025-08-28 15:47:47 +0000

    Simplify, add instructions for copilot, add main file

--- main.js
+++ main.js
@@ -4,11 +4,9 @@ import { TextInterface } from "text-interface";
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
 let ti = new TextInterface(app, "Example Text Interface");
+
 ti.output("What is your name?");
 let name = await ti.readText();
 await ti.output("Hello, " + name);
-await ti.output("Here is a pretty mountain:");
-await ti.showImage(
-  "https://upload.wikimedia.org/wikipedia/commons/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg"
-);
-await ti.readText();
+
+
